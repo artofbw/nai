b5e55a8679c7c6ff9eed361b0dde6e60a0b6e3b1
Łukasz Ćwikliński <artofbw@gmail.com>
2021-12-15 00:45:14 +0100

    clothes

--- 05_nlp_for_classifier/clothes.py
+++ 05_nlp_for_classifier/clothes.py
@@ -8,24 +8,83 @@ pip3 install -r requirements.txt
 '''
 
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
-cifar10 = tf.keras.datasets.cifar10
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+def plot_image(i, predictions_array, true_label, img):
+    true_label, img = true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
 
-x_train, x_test = x_train / 255.0, x_test / 255.0
+    plt.imshow(img, cmap=plt.cm.binary)
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(24, 3, activation='relu', padding='same', input_shape=(32, 32, 3)),
-    tf.keras.layers.Flatten(),
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                         100*np.max(predictions_array),
+                                         class_names[true_label]),
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    true_label = true_label[i]
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10, activation='softmax')
+    tf.keras.layers.Dense(10)
 ])
 
-model.summary()
+model.compile(
+    optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy']
+)
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=10)
 
-model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
 
 probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+
+i = 2
+img = test_images[i]
+img = (np.expand_dims(img, 0))
+predictions_single = probability_model.predict(img)
+
+plot_value_array(1, predictions_single[0], test_labels)
+_ = plt.xticks(range(10), class_names, rotation=45)
+plt.show()
+
+print(np.argmax(predictions_single[0]))
